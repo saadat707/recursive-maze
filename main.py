63837bd3fa7489736f23aee709df2197ee507fc3
saadat707 <sadot9079@gmail.com>
2025-04-13 20:56:03 +0600

    Добавлен ввод размера лабиринта от пользователя.

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ from maze_generator import generate_maze, print_maze
 from pathfinder import dfs
 
 def main():
-    width, height = 10, 10  # Размер лабиринта
+    # Ввод размера лабиринта от пользователя
+    try:
+        width = int(input("Введите ширину лабиринта: "))
+        height = int(input("Введите высоту лабиринта: "))
+    except ValueError:
+        print("Пожалуйста, введите целые числа для ширины и высоты.")
+        return
+
+    if width < 5 or height < 5:
+        print("Размер лабиринта должен быть не менее 5x5.")
+        return
+
     maze, start, end = generate_maze(width, height)
     
-    print("Сгенерированный лабиринт:")
+    print("\nСгенерированный лабиринт:")
     print_maze(maze)
     
     path = []
@@ -14,13 +25,13 @@ def main():
         print(path)
 
         # Отметить путь в лабиринте точками
-for x, y in path:
-    if (x, y) != start and (x, y) != end:  # Убедимся, что старт и финиш не заменяются
-        maze[y][x] = '.'  # Помечаем путь точками
+        for x, y in path:
+            if (x, y) != start and (x, y) != end:  # Убедимся, что старт и финиш не заменяются
+                maze[y][x] = '.'  # Помечаем путь точками
 
-# Выводим лабиринт с помеченным путём
-print("\nЛабиринт с найденным путём:")
-print_maze(maze)
+        # Выводим лабиринт с помеченным путём
+        print("\nЛабиринт с найденным путём:")
+        print_maze(maze)
     else:
         print("\nПуть не найден.")
 
